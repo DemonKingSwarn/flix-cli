fc3bb9e276a473aba82647591680b41119cc74b7
DemonKingSwarn <rockingswarn@gmail.com>
2022-06-13 19:39:33 +0530

    for fox sake

---
+++ runner.py
@@ -0,0 +1,4 @@
+from flix_cli.__main__ import __flix_cli__
+
+if __name__ == "__main__":
+    __flix_cli__()
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=requirements,
     entry_points="""
         [console_scripts]
-        flix-cli=flix_cli:__main__.__flixcli__
+        flix-cli=flix_cli.__main__:__flix_cli__
     """,
     include_package_data=True,
     )
