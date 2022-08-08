0bec3bc8a1767273d3fe569efb4ddcc69ba8a069
DemonKingSwarn <rockingswarn@gmail.com>
2022-08-08 12:38:03 +0530

    Chore: minor changes

--- flix_cli/core/utils/__player__.py
+++ flix_cli/core/utils/__player__.py
@@ -1,11 +1,14 @@
+import platform
 import subprocess
 
 MPV_EXECUTABLE = "mpv"
 IINA_EXECUTABLE = "iina"
 
+MPV_INTENT = "am start -a \"android.intent.action.VIEW\""
+
 def play(file, name, referer, subtitles):
     try:
-        try:
+        if(platform.system() == "Linux"):
             args = [
                 MPV_EXECUTABLE,
                 file,
@@ -18,7 +21,7 @@ def play(file, name, referer, subtitles):
 
             mpv_process.wait()
 
-        except Exception as e:
+        elif(platform.system() == "Darwin"):
             args = [
                 IINA_EXECUTABLE,
                 f"--mpv-referrer={referer}",
@@ -33,7 +36,11 @@ def play(file, name, referer, subtitles):
 
             iina_process.wait()
 
+        elif(subprocess.check_output(['uname', '-o']).strip() == b'Android'):
+            subprocess.call(f"curl -s {subtitles} -o /storage/emulated/0/tmp.srt", shell=True)
+            subprocess.call(f"{MPV_INTENT} -d {file} --eu \"subs\" \"/storage/emulated/0/tmp.srt\" --esn \"subs.enable\" --ei \"decode_mode\" 2 -n \"is.xyz.mpv/.MPVActivity\"", shell=True)
+
     except Exception as e:
-        print("[!] mpv or iina not found.")
+        print("[!] no supported video player were found.")
         exit(1)
 
