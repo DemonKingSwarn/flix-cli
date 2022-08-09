764d30fab7fb6a938619151693d71deeafae026f
DemonKingSwarn <rockingswarn@gmail.com>
2022-08-09 14:36:22 +0530

    Chore: Version bump `1.3.9` -> `1.4.0`

---
+++ build/lib/flix_cli/__main__.py
@@ -0,0 +1,7 @@
+from .core import __flix_cli__
+
+def __flixcli__():
+    __flix_cli__.fetch
+
+if __name__ == "__main__":
+    __flixcli__()
--- build/lib/flix_cli/core/utils/__player__.py
+++ build/lib/flix_cli/core/utils/__player__.py
@@ -8,7 +8,11 @@ VLC_INTENT = "am start -a \"android.intent.action.VIEW\""
 
 def play(file, name, referer, subtitles):
     try:
-        if(platform.system() == "Linux"):
+        if(subprocess.check_output(['uname', '-o']).strip() == b'Android'):
+            subprocess.call(f"curl -sL {subtitles} -o /storage/emulated/0/subs.srt", shell=True)
+            subprocess.call(f"{VLC_INTENT} -d {file} -n org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity -e \"title\" \"Playing {name}\" -e \"subtitles_location\" \"/storage/emulated/0/subs.srt\"", shell=True)
+
+        elif(platform.system() == "Linux"):
             args = [
                 MPV_EXECUTABLE,
                 file,
@@ -16,6 +20,7 @@ def play(file, name, referer, subtitles):
                 f"--force-media-title=Playing {name}",
             ]
             args.extend(f"--sub-file={_}" for _ in subtitles)
+            print(f"{_}" for _ in subtitles)
 
             mpv_process = subprocess.Popen(args, stdout=subprocess.DEVNULL)
 
